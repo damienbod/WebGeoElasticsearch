00dc3183a58495eca0d8e468053ecb07daff3040
damienbod <damien_bod@hotmail.com>
2024-09-13 20:53:07 +0200

    clean up

--- WebGeoElasticsearch/Program.cs
+++ WebGeoElasticsearch/Program.cs
@@ -4,18 +4,14 @@ builder.AddServiceDefaults();
 
 builder.AddElasticsearchClient("elasticsearch");
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
