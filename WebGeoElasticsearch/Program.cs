0f3ebfbd19d52c76bd9926a526995a64b24bd394
damienbod <damien_bod@hotmail.com>
2024-09-13 20:44:13 +0200

    Add elasticsearch client

--- WebGeoElasticsearch/Program.cs
+++ WebGeoElasticsearch/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+builder.AddElasticsearchClient("elasticsearch");
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
