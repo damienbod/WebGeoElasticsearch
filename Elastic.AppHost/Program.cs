9496ea64d574d819a9257f8e4aa0141271edba55
damienbod <damien_bod@hotmail.com>
2024-09-13 19:39:15 +0200

    Init Elasticsearch docker

---
+++ Elastic.AppHost/Program.cs
@@ -0,0 +1,15 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var passwordElastic = builder.AddParameter("passwordElastic", secret: true);
+
+var elasticsearch = builder.AddElasticsearch("elasticsearch", password: passwordElastic)
+    .WithDataVolume()
+    .RunElasticWithHttpsDevCertificate(port: 9200);
+
+//var apiService = builder.AddProject<Projects.Elastic_ApiService>("apiservice");
+
+//builder.AddProject<Projects.Elastic_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(apiService);
+
+builder.Build().Run();
