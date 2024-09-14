94d83890f2c496dc0ea109379ecf0d253412a80c
damienbod <damien_bod@hotmail.com>
2024-09-14 11:38:20 +0200

    Clean up setup

--- WebGeoElasticsearch/ElasticsearchApi/SearchProvider.cs
+++ WebGeoElasticsearch/ElasticsearchApi/SearchProvider.cs
@@ -1,18 +1,35 @@
 ﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
 using WebGeoElasticsearch.Models;
 
 namespace WebGeoElasticsearch.ElasticsearchApi;
 
 public class SearchProvider
 {
-    public void AddMapDetailData()
+    private const string IndexName = "mapdetails";  
+
+    public async Task AddMapDetailDataAsync()
     {
         var dotNetGroup = new MapDetail { DetailsCoordinates = GeoLocation.Coordinates([7.47348, 46.95404]), Id = 1, Name = ".NET User Group Bern", Details = "http://www.dnug-bern.ch/", DetailsType = "Work" };
         var dieci = new MapDetail { DetailsCoordinates = GeoLocation.Coordinates([7.41148, 46.94450]), Id = 2, Name = "Dieci Pizzakurier Bern", Details = "http://www.dieci.ch", DetailsType = "Pizza" };
         var babylonKoeniz = new MapDetail { DetailsCoordinates = GeoLocation.Coordinates([7.41635, 46.92737]), Id = 3, Name = "PIZZERIA BABYLON Köniz", Details = "http://www.pizza-babylon.ch/home-k.html", DetailsType = "Pizza" };
         var babylonOstermundigen = new MapDetail { DetailsCoordinates = GeoLocation.Coordinates([7.48256, 46.95578]), Id = 4, Name = "PIZZERIA BABYLON Ostermundigen", Details = "http://www.pizza-babylon.ch/home-o.html", DetailsType = "Pizza" };
-        
-        // TODO add data
+
+        var settings = new ElasticsearchClientSettings(new Uri("https://localhost:9200"))
+          .Authentication(new BasicAuthentication("elastic", "Password1!"));
+
+        var client = new ElasticsearchClient(settings);
+
+        var exist = await client.Indices.ExistsAsync(IndexName);
+        if (exist.Exists)
+        {
+            await client.Indices.DeleteAsync(IndexName);
+        }
+
+        var response = await client.IndexAsync(dotNetGroup, IndexName, "1");
+        response = await client.IndexAsync(dieci, IndexName, "2");
+        response = await client.IndexAsync(babylonKoeniz, IndexName, "3");
+        response = await client.IndexAsync(babylonOstermundigen, IndexName, "4");
     }
 
     //{
